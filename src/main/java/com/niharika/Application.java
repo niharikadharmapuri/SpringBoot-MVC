2888e9dfe7a98ead72d5212419aa8e0f51fa3bbc
niharikadharmapuri <niharika.dharmapuri@gmail.com>
2018-10-02 23:00:20 -0500

    project structure

---
+++ src/main/java/com/niharika/Application.java
@@ -0,0 +1,13 @@
+package com.niharika;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
