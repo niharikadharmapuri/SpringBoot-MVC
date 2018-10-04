83e7738cad1ef48c034c524615b4dc22435a0a63
niharikadharmapuri <niharika.dharmapuri@gmail.com>
2018-10-04 12:01:01 -0500

    swagger ui and doc

--- src/main/java/com/niharika/App.java
+++ src/main/java/com/niharika/App.java
@@ -3,9 +3,11 @@ package com.niharika;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class App
 {
     public static void main( String[] args ){
